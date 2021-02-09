5f6e328c4caaf5075796fc406c4d1b7d5205a241
jakka351 <57064943+jakka351@users.noreply.github.com>
2021-02-09 20:15:50 +1100

    Update swc_phone.sh

--- swc_scripts/swc_phone.sh
+++ swc_scripts/swc_phone.sh
@@ -3,9 +3,8 @@
 #steering wheel controls
 
 #cansend swc_phone
-cansend can0 2F2#02.E3.06.4E.08.1D.68.00
-#candump can0,2F2:2E3064E081D6800
-
+cansend can0 2F2#00.00.00.00.00.00.61.00
+#candump can0,2F2:1fffffff
 
 
 
