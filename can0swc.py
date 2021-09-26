1e8757516faa6cb1fa791bcc68502931b69e33d6
J.L <57064943+jakka351@users.noreply.github.com>
2021-09-27 01:55:25 +1000

    Update can0swc.py

--- can0swc.py
+++ can0swc.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 # can0swc fg falcon swc-can adapter
 # https://github.com/jakka351/FG-Falcon | https://github.com/jakka351/can0swc
-# 
+# assumes ms-can is up on can0
+# buffers several CAN IDS, then matches byte specific data
+# when it matches, emits keypress
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <https://www.gnu.org/licenses/>. 
 ############################
-#import modules
+# Import modules
 ############################
 import can
 import time
@@ -13,7 +28,7 @@ import queue
 from threading import Thread
 import sys, traceback
 ############################
-# Define Keypresses
+# Define Variables
 ############################
 device = uinput.Device([
         uinput.KEY_N,
@@ -22,36 +37,54 @@ device = uinput.Device([
         uinput.KEY_C,
         uinput.KEY_W,
         ])
-############################
-#Global Variables
-############################
-
 c                      = ''
 count                  = 0  
 # CAN Id's
+#2E2                   = 0x2E2
+#2E3                   = 0x2E3
 SWC                    = 0x2F2              #id 751
 ICC                    = 0x2FC              #id 764
 BEM                    = 0x307
-INTACTTEMP             = 0x313
+#IAT                    = 0x313
+#                      = 0x311
+#                      = 0x315
+#VIN                    = 0x317
+#                      = 0x30B
+#                      = 0x30D
+#                      = 0x30F
+
 # SWC Button CAN Data
 SWC_SEEK               = (0x08, 0x09, 0x0C)  # seek button on byte [7] of id 0x2f2
 SWC_VOLUP              = (0x10, 0x11, 0x14)  # volume + button on bit [7] of id 0x2f2
 SWC_VOLDOWN            = (0x18, 0x19, 0x1C)  # volume - button on bit [7] of id 0x2f2
-SWC_PHONE              = (0x61, 0x65, 0x68)  # phone button on bit [6] of id 0x2f2
+SWC_PHONE              = (0x61, 0x65, 0x68)  # phone button on byte [6] of id 0x2f2
+#SWC_MODE              = (0x00,0x00)
 # ICC Button CAN Data
-ICC_VOLUP              = 0x41 # vol + button on bit [3] of id 0x2fc
-ICC_VOLDOWN            = 0x81 # vol - button on bit [3] of id 0x2fc
-ICC_NEXT               = 0x04 # seek + button on bit [0] of id 0x2fc
-ICC_PREV               = 0x08 # seek - button on bit [0] of id 0x2fc
-ICC_EJECT              = 0x80 # eject button on bit [1] of id 0x2fc
-ICC_LOAD               = 0x40 # load button on bit [1] of id 0x2fc
+ICC_VOLUP              = 0x41 # vol + button on byte [3] of id 0x2fc
+ICC_VOLDOWN            = 0x81 # vol - button on byre [3] of id 0x2fc
+ICC_NEXT               = 0x04 # seek + button on byte [0] of id 0x2fc
+ICC_PREV               = 0x08 # seek - button on byte [0] of id 0x2fc
+ICC_EJECT              = 0x80 # eject button on byte [1] of id 0x2fc
+ICC_LOAD               = 0x40 # load button on byte [1] of id 0x2fc
 #BEM Butonn CAN Data
 BEM_LOCK               = 0x00
 BEM_UNLOCK             = 0x00
 BEM_DSC                = 0x00
 BEM_DOMELIGHT          = 0x90
 BEM_HAZARD             = 0x01
-
+"""
+AudioCurrentMediaMode  = can.Message(arbitration_id=SWC, data=[0x6a, 0x61, 0x6b, 0x6b, 0x61, 0x33, 0x35, 0x31], is_extended_id=False)
+IccMediaControls       = can.Message(arbitration_id=ICC, data=[0x6a, 0x61, 0x6b, 0x6b, 0x61, 0x33, 0x35, 0x31], is_extended_id=False)
+IccBemControls         = can.Message(arbitration_id=BEM, data=[0x6a, 0x61, 0x6b, 0x6b, 0x61, 0x33, 0x35, 0x31], is_extended_id=False)
+IntActTemp             = can.Message(arbitration_id=IAT, data=[0x6a, 0x61, 0x6b, 0x6b, 0x61, 0x33, 0x35, 0x31], is_extended_id=False)
+BodyElecSetting        = can.Message(arbitration_id=, data=[0x6a, 0x61, 0x6b, 0x6b, 0x61, 0x33, 0x35, 0x31], is_extended_id=False)
+CdMp3Player            = can.Message(arbitration_id=, data=[0x6a, 0x61, 0x6b, 0x6b, 0x61, 0x33, 0x35, 0x31], is_extended_id=False)
+IpodPlayingAux2        = can.Message(arbitration_id=, data=[0x6a, 0x61, 0x6b, 0x6b, 0x61, 0x33, 0x35, 0x31], is_extended_id=False)
+IpodSerialData         = can.Message(arbitration_id=, data=[0x6a, 0x61, 0x6b, 0x6b, 0x61, 0x33, 0x35, 0x31], is_extended_id=False)
+IpodRemoteControl      = can.Message(arbitration_id=, data=[0x6a, 0x61, 0x6b, 0x6b, 0x61, 0x33, 0x35, 0x31], is_extended_id=False)
+AcmKeepAliveSignal     = can.Message(arbitration_id=, data=[0x6a, 0x61, 0x6b, 0x6b, 0x61, 0x33, 0x35, 0x31], is_extended_id=False)
+VehicleIdentify        = can.Message(arbitration_id=VIN, data=[0x6a, 0x61, 0x6b, 0x6b, 0x61, 0x33, 0x35, 0x31], is_extended_id=False)
+"""
 def scroll():
     #prints logo to console
         print('  ')
@@ -141,10 +174,25 @@ def cleanline():                      # cleans the last output line from the con
 def cleanscreen():                    # cleans the whole console screen
     os.system("clear")
 
-
-#def settings():
-#        message_setting = 
-
+def displaytext():
+    RDS                    = 0x309 # Display Text on FDIM & Cluster
+    RadioStation           = can.Message(arbitration_id=RDS, data=[0x6a, 0x61, 0x6b, 0x6b, 0x61, 0x33, 0x35, 0x31], is_extended_id=False)
+    task0x309 = bus.send_periodic(RadioStation, 0.01)
+    assert isinstance(task0x309, can.CyclicSendTaskABC)
+    time.sleep(60)
+    task.stop()
+    
+def cleardtc():
+    ICdiag               = 0x720 # Display Text on FDIM & Cluster
+    ClearICdiag          = can.Message(arbitration_id=ICdiag, data=[0x03, 0x14, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00], is_extended_id=False)
+    bus.send(ClearICdiag, timeout=None)
+    FDIMdiag             = 0x7A6 # Display Text on FDIM & Cluster
+    ClearFDIMdiag        = can.Message(arbitration_id=FDIMdiag, data=[0x03, 0x14, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00], is_extended_id=False)
+    bus.send(ClearFDIMdiag, timeout=None)
+    BEMdiag              = 0x726 # Display Text on FDIM & Cluster
+    ClearBEMdiag         = can.Message(arbitration_id=BEMdiag, data=[0x03, 0x14, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00], is_extended_id=False)
+    bus.send(ClearBEMdiag, timeout=None)
+    
 def main():
     try:
         while True:
@@ -170,7 +218,7 @@ def main():
                         print("SWCVolUpBtn pushed @", message.timestamp)
                         time.sleep(0.2)
                         
-                    elif message.data[7] == SWC_VOLDOWN:
+                    elif message.data[7] in SWC_VOLDOWN:
                         #device.emit_click(uinput.KEY_VOLUMEDOWN) #voldown openauto
                         cleanline()
                         cleanline()
@@ -223,7 +271,7 @@ def main():
                         time.sleep(0.2)
                         
                     elif message.data[1] == ICC_LOAD:
-                        #os.system("sudo systemctl start dash.service")
+                        cleardtc()    #clears fault codes
                         cleanline()
                         cleanline()
                         print(message)
@@ -231,7 +279,7 @@ def main():
                         time.sleep(0.2)
                         
                     elif message.data[1] == ICC_EJECT:
-                        #os.system("sudo systemctl stop dash.service")
+                        displaytext()  #displays custom text for 60 seconds on FDIM and Cluster
                         cleanline()
                         cleanline()
                         print(message)
@@ -241,47 +289,49 @@ def main():
                     else:
                         pass
                     
-                elif message.arbitration_id == BEM and message.data[3] == BEM_LOCK:
-                    #os.system("omxplayer lock.mp4 ")
-                    cleanline()
-                    cleanline()
-                    print(message)
-                    print("VehicleLockButton pushed @", message.timestamp)
-                    time.sleep(2.0)
-                    
-                elif message.arbitration_id == BEM and message.data[3] == BEM_UNLOCK:
-                    #os.system("omxplayer ")
-                    cleanline()
-                    cleanline()
-                    print(message)
-                    print("VehicleUnlockButton pushed @", message.timestamp)
-                    
-                elif message.arbitration_id == BEM and message.data[3] == BEM_DSC:
-                    time.sleep(1.5)
-                    if message.arbitration_id == BEM and message.data[3] == BEM_DSC:
-                        #os.system("omxplayer burnout.mp4")
+                elif message.arbitration_id == BEM:
+                    if message.data[3] == BEM_LOCK:
+                        #os.system("omxplayer lock.mp4 ")
                         cleanline()
                         cleanline()
                         print(message)
-                        print("DynamicStabilityControlSwitch pushed @", message.timestamp)
-                    else:
-                        pass
-                
-                elif message.arbitration_id == BEM and message.data[3] == BEM_DOMELIGHT:
-                    #os.system("omxplayer whitelight.mp4")
-                    cleanline()
-                    cleanline()
-                    print(message)
-                    print("DomeLightSwitch pushed @", message.timestamp) 
+                        print("VehicleLockButton pushed @", message.timestamp)
+                        time.sleep(2.0)
                     
-                elif message.arbitration_id == BEM and message.data[3] == BEM_HAZARD:
-                    #os.system("omxplayer /home/pi/hazard.mp4 ")
-                    cleanline()
-                    cleanline()
-                    print(message)
-                    print("HazardLightSwitch pushed @", message.timestamp) 
-                    time.sleep(3)
-                        
+                    elif message.data[3] == BEM_UNLOCK:
+                        #os.system("omxplayer ")
+                        cleanline()
+                        cleanline()
+                        print(message)
+                        print("VehicleUnlockButton pushed @", message.timestamp)
+                    
+                    elif message.data[3] == BEM_DSC:
+                        time.sleep(1.0)
+                        if message.data[3] == BEM_DSC:
+                            #os.system("omxplayer burnout.mp4")
+                            cleanline()
+                            cleanline()
+                            print(message)
+                            print("DynamicStabilityControlSwitch pushed @", message.timestamp)
+                        else:
+                            pass
+                
+                    elif message.data[3] == BEM_DOMELIGHT:
+                        #os.system("omxplayer whitelight.mp4")
+                        cleanline()
+                        cleanline()
+                        print(message)
+                        print("DomeLightSwitch pushed @", message.timestamp) 
+                    
+                    elif message.data[3] == BEM_HAZARD:
+                        #os.system("omxplayer /home/pi/hazard.mp4 ")
+                        cleanline()
+                        cleanline()
+                        print(message)
+                        print("HazardLightSwitch pushed @", message.timestamp) 
+                        time.sleep(3)
+
+                """        
                 elif message.arbitration_id == INTACTTEMP and message.data[0] <= 0x01:
                     IntActTemp = float(message.data[0])
                     if IntActTemp != 0:
@@ -292,10 +342,11 @@ def main():
                         time.sleep(3.0)
                     else:
                         pass
-
+                """
                 else:
                     pass
-                                                                           
+                
+
     except KeyboardInterrupt:
         sys.exit(0)                                              # quit if ctl + c is hit
     except Exception:
